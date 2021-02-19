64f73fed03a400e4b55d4ee07b2df4fb34d63d38
tjlr50 <tjlr50@gmail.com>
2021-02-19 00:57:04 -0500

    Menu Create

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ Resturante puede ver estado de un pedido
 Rutas Usuario:
 login
 logup
-idx (el muro)
-dashboard -> ver ordenes
-orders/ orders/:id order/create
+idx (Ver todos los restaurantes)
+orders/ order/create
 
 Rutas Restaurant:
 login
 logup
-dashboard -> ver ordenes
-menus/ menus/:id menus/create
+idx (Ver todas las ordenes a mis menus)
+menus/ menus/create menus/edit/:id menus/delete/:id
 
 set MONGO_URI=mongodb://localhost:27017/telemenu
 set SECRET=cambiar
@@ -28,16 +27,26 @@ set SECRET=cambiar
 
 """
 
+from models import Menu, Order, Restaurant, User
 import datetime
+import functools
 import os
 
 import bcrypt
+import cloudinary
+import cloudinary.api
+import cloudinary.uploader
 import pymongo.errors
+from bson.objectid import ObjectId
 from flask import Flask, redirect, request, session
 from flask.templating import render_template
 from pymodm.errors import ValidationError
 
-from models import Menu, Order, Restaurant, User
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_CLOUD_SECRET')
+)
 
 
 SECRET_KEY = os.environ.get('SECRET')
@@ -51,6 +60,24 @@ def format_date(value, format="%B %d"):
 
 
 app.jinja_env.filters['format_date'] = format_date
+
+
+def user_logged_in(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'user' not in session:
+            return redirect('/user/login')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def restaurant_logged_in(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'restaurant' not in session:
+            return redirect('/restaurant/login/')
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @app.route('/user/login', methods=['GET', 'POST'])
@@ -108,14 +135,7 @@ def order():
 
 @app.route('/orders/create')
 def order_create():
-    pass  # -> noop
-
-# Comprobar si es usuario o restaurante mediante sesión, ofrecer login si no hay sesión
-
-
-@app.route('/dasboard')
-def dashboard():
-    pass
+    pass  # -> noo
 
 
 @app.route('/restaurant/login', methods=['GET', 'POST'])
@@ -132,7 +152,7 @@ def restaurant_login():
                 return redirect('/')
             else:
                 return render_template('restaurant/login.html', wrong_pass=True)
-        except User.DoesNotExist:
+        except Restaurant.DoesNotExist:
             return render_template('restaurant/login.html', no_restaurant=True)
 
 
@@ -154,12 +174,49 @@ def restaurant_logup():
     return redirect('/restaurant/login')
 
 
-# Comprobar si es usuario o restaurante mediante sesión, ofrecer login si no hay sesión
+@app.route('/menus')
+@restaurant_logged_in
+def menus():
+    return render_template('menus/index.html', menus=Restaurant.objects.only('menus').get(
+        {'_id': session['restaurant']}).menus)
+
+
+@app.route('/menus/create', methods=['GET', 'POST'])
+@restaurant_logged_in
+def menus_create():
+    if request.method == "GET":
+        return render_template('menus/create.html')
+    else:
+        restaurant = Restaurant.objects.get({'_id': session['restaurant']})
+        try:
+            composition = request.form['composition']
+            price = float(request.form['price'])
+            valid_date = datetime.datetime.strptime(
+                request.form['valid_date'], '%Y-%m-%d')
+            photo = request.files['photo']
+            photo_url = cloudinary.uploader.upload_image(photo).build_url()
+            menu = Menu(ObjectId(), composition, price, valid_date, photo_url)
+            restaurant.menus.append(menu)
+            restaurant.save()
+        except Exception as e:
+            print(e)
+            restaurant.menus.pop()
+            return render_template('menus/create.html', error=True)
+        return render_template('menus/index.html')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', restaurants=list(Restaurant.objects.all()))
+    orders = []
+    if 'restaurant' in session:
+        menus = Restaurant.objects.only('menus').get(
+            {'_id': session['restaurant']}).menus
+        menus_ids = [menu._id for menu in menus]
+        for id in menus_ids:
+            sub_orders = Order.objects.get_queryset().raw({'menu': id})
+            orders.extend(sub_orders)
+    print(orders)
+    return render_template('index.html', restaurants=Restaurant.objects.all(), orders=orders)
 
 
 def main():
